0026cc82b9bde34b78dee874eb82fd6f7f0d86c5
unknown <lidanyang1111@gmail.com>
2022-01-08 12:14:22 -0800

    version new on 01/08/2022

---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+class Todo extends React.Component {
+    handleClick = () => {
+        console.log("handle clicked",this.props.todo.id);
+        this.props.handleToggle(this.props.todo.id);
+    }
+
+    render() {
+        return (
+            <li onClick={this.handleClick}>
+                {this.props.todo.task} {this.props.todo.completed? <span> - completed </span> : <span></span>}
+            </li>
+
+        )
+    }
+}
+
+export default Todo;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            input:""
+        }
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.handleAdd(this.state.input);
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            ...this.state,
+            input:event.target.value
+        })
+    }
+
+    render() {
+        return (           
+            <form>
+                <input onChange={this.handleChange}/>
+                <button onClick={this.handleSubmit}>Add</button>
+            </form>
+        )
+    }
+}
+
+export default TodoForm;
